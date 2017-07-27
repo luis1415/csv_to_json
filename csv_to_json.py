31ad8708e567536104ca5b9b3a2090349d6a6ecb
luis1415 <luisbermudeztorres@gmail.com>
2017-07-27 08:45:11 -0500

    csv lessons

--- csv_to_json.py
+++ csv_to_json.py
@@ -4,29 +4,44 @@ import pprint
 import io
 
 
-def csv_to_json(archivo):
+def csv_to_json(archivo, clase, columnas):
     """
     Esta funcion toma como argumento el nombre del archivo con la extension .csv y lo pasa a formato .json
-    clasificandolos por la columna Topic
+    clasificandolos y sacando un archivo por la columna clase
     :param archivo: nombre del archivo
+    :param clase: la columna del csv por la cual se clasifican los archivos
+    :param columnas: las columnas que se filtran en el dataframe
     """
     df = pd.read_csv(archivo)
-    topics = list(df['Topic'].unique())
+    topics = list(df[clase].unique())
     topics = [x for x in topics if str(x) != 'nan']
 
     for topic in topics:
-        df_ = df[df['Topic'] == topic]
+        df_ = df[df[clase] == topic]
 
-        df_ = df_[['Spanish', 'English', 'French', 'Chinese', 'EnglishAudio', 'SpanishAudio', 'frenchaudio',
-                   'chineseaudio', 'Image']]
+        df_ = df_[columnas]
 
         df_ = df_.to_json(orient='records')
         data_json = json.loads(df_)
-        pprint.pprint(data_json)
+        pprint.pprint(data_json[0])
+        data_json = {'items': data_json}
         print(topic)
 
         with io.open('{}.json'.format(topic), 'w', encoding='utf-8') as f:
             f.write(json.dumps(data_json, ensure_ascii=False, indent=4, sort_keys=True))
 
+
 if __name__ == '__main__':
-    csv_to_json('Oasis.csv')
+    # para el primer archivo de oasis
+    columnas_oasis = ['Spanish', 'English', 'French', 'Chinese', 'EnglishAudio', 'SpanishAudio', 'frenchaudio',
+                      'chineseaudio', 'Image']
+    csv_to_json('Oasis.csv', 'Topic', columnas_oasis)
+
+    # para las lecciones
+    columnas_lessons = ['Spanish', 'English', 'French', 'Chinese', 'SpanishAudio', 'EnglishAudio',
+                        'FrenchAudio', 'ChineseAudio', 'Image']
+    csv_to_json('lesson1.csv', 'Lesson ID', columnas_lessons)
+    csv_to_json('lesson2.csv', 'Lesson ID', columnas_lessons)
+    csv_to_json('lesson3.csv', 'Lesson ID', columnas_lessons)
+
+
